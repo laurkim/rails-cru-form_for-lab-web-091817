3f4dc4f16bffdd2cfbc44351527dd0ec1f0eae23
laurkim <laurakeem@gmail.com>
2017-10-16 17:32:45 -0400

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,40 @@
+class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    # byebug
+    @artist = Artist.create(artist_params(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    # byebug
+    @artist.update(artist_params(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  private
+
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
+
+    def artist_params(*args)
+      params.require(:artist).permit(*args)
+    end
+
+end
